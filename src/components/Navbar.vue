ea3117100ba9ec6a65a962b849308ce1a5691c76
SoaAlex <l.soaresalex@gmail.com>
2020-12-31 00:06:16 +0100

    Init Vue (Vue CLI Hard mode)

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- Just an image -->
+    <b-navbar variant="info" type="dark">
+      <b-navbar-brand href="#">
+        <img
+          alt="Biostart logo"
+          src="../assets/Logo-biostart.png"
+          width="150px"
+          style="margin-left: -10%; margin-bottom: -5%; margin-top: -5%"
+        />
+      </b-navbar-brand>
+
+      <b-navbar-nav class="ml-auto">
+        {{ new Date().toLocaleString() }}
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
