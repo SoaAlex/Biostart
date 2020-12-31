fe6ba3cff4cde73b8a6727082c1119e2daa11207
SoaAlex <l.soaresalex@gmail.com>
2020-12-31 17:24:34 +0100

    First tests with Vue and monitoring interface

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,18 @@
 <template>
-  <div>
+  <div class="Navbar">
     <!-- Just an image -->
     <b-navbar variant="info" type="dark">
-      <b-navbar-brand href="#">
+      <b-navbar-brand>
         <img
           alt="Biostart logo"
           src="../assets/Logo-biostart.png"
-          width="150px"
-          style="margin-left: -10%; margin-bottom: -5%; margin-top: -5%"
+          class="logo"
         />
       </b-navbar-brand>
+      <b-nav-text class="nav-text">Monitoring</b-nav-text>
 
       <b-navbar-nav class="ml-auto">
-        {{ new Date().toLocaleString() }}
+        <b-nav-text class="nav-text">{{ timestamp }}</b-nav-text>
       </b-navbar-nav>
     </b-navbar>
   </div>
@@ -20,6 +20,47 @@
 
 <script>
 export default {
-  name: "Navbar"
+  name: "Navbar",
+  el: "#Navbar",
+  data: function() {
+    return {
+      timestamp: ""
+    };
+  },
+  created() {
+    setInterval(this.getNow, 1000);
+  },
+  methods: {
+    getNow: function() {
+      var today = new Date();
+      var date =
+        today.getFullYear() +
+        "-" +
+        (today.getMonth() + 1) +
+        "-" +
+        today.getDate();
+      var currentHours = ("0" + today.getHours()).substr(-2);
+      var currentMins = ("0" + today.getMinutes()).substr(-2);
+      var currentSecs = ("0" + today.getSeconds()).substr(-2);
+      var time = currentHours + ":" + currentMins + ":" + currentSecs;
+      var dateTime = date + " | " + time;
+      this.timestamp = dateTime;
+    }
+  }
 };
 </script>
+
+<style scoped>
+.logo {
+  margin-left: -10%;
+  margin-bottom: -5%;
+  margin-top: -5%;
+  width: 150px;
+}
+
+.nav-text {
+  color: #000000 !important;
+  font-size: 20px;
+  font-family: Monaco, monospace !important;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,40 @@
 <template>
-  <div class="home">
+  <div class="Home">
     <Navbar />
+    <b-container class="bordered">
+      <b-row align-v="stretch">
+        <b-col class="bordered" cols="8"><PressureChart /></b-col>
+        <b-col class="bordered">Total Volume filtered</b-col>
+      </b-row>
+      <b-row>
+        <b-col class="bordered" cols="8"> </b-col>
+        <b-col class="bordered">Total something</b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from "@/components/Navbar.vue";
+import PressureChart from "@/components/PressureChart.vue";
 
 export default {
   name: "Home",
   components: {
-    Navbar
+    Navbar,
+    PressureChart
   }
 };
 </script>
+
+<style scoped>
+.bordered {
+  border: solid 1px;
+  width: 100%;
+}
+
+.fullrow {
+  width: 100%;
+}
+</style>
